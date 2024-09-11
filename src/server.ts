eaf2fd32dfb6294de1a1a084d6183a688f79eea6
samllban <137834390+samllban@users.noreply.github.com>
2024-09-11 11:44:22 -0300

    update project

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,18 @@ const app = express();
 
 const port = 3333;
 
+app.get("/", (req, res) => {
+    return res.json({
+        message: "Hello, World!"
+    })
+})
+
+app.post("/users",(req, res) => {
+    return res.json({
+        messesge:"usuario salvo com secesso"
+    })
+})
+
 app.listen(3333, () => 
-    console.log(`server is running on port <http://localhost${port}`)
+    console.log(`server is running on port <http://localhost:${port}`)
 )
